765461419d617aa37a23b9d5d91bb5a5066dce19
konradsic <102176820+konradsic@users.noreply.github.com>
2022-04-12 20:00:35 +0200

    Better cactus

--- script.js
+++ script.js
@@ -15,7 +15,13 @@ function DrawGround() {
 }
 function DrawCactus(xpos, cact_size_x, cact_size_y) {
     ctx.fillStyle = "#2db700";
-    ctx.fillRect(xpos, (0.8*board.height), -1*cact_size_x, -1*cact_size_y)
+    ctx.fillRect(xpos, (0.8*board.height), -1*cact_size_x, -1*cact_size_y);
+
+    ctx.fillRect(xpos, ((0.8*board.height)+30-cact_size_y), 30, 10);
+    ctx.fillRect(xpos+20, ((0.8*board.height)+40-cact_size_y), 10, -30);
+
+    ctx.fillRect(xpos-50, ((0.8*board.height)+60-cact_size_y), 20, 8);
+    ctx.fillRect(xpos-50, ((0.8*board.height)+68-cact_size_y), 8, -20);
 }
 function Game() {
     DrawBoard();
