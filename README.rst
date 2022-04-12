b42a00c6458cc26ebf2a6599494b8ee9e9767095
konradsic <102176820+konradsic@users.noreply.github.com>
2022-04-12 19:42:28 +0200

    Animated cactus

--- README.rst
+++ README.rst
@@ -1,6 +1,7 @@
 dino-game
 ----------
 A simple Dino game. Dino jumps and slides avoiding cactuses!
+
 Version
 ----------
-v1.1 - Added canvas.
+v1.5 - Moving cactus
--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 let board, ctx;
 
-board = document.getElementById('game-board');
-ctx = board.getContext('2d');
+board = document.getElementById("game_board");
+ctx = board.getContext("2d");
 
-ctx.fillStyle = "#434343";
-ctx.fillRect(0, (ctx.canvas.width*0.8), ctx.canvas.width, (ctx.canvas.width*0.8))
+let cactus_xpos = board.width - 30;
+
+function DrawBoard() {
+    ctx.fillStyle = "#dddddd";
+    ctx.fillRect(0,0,board.width, board.height);
+}
+function DrawGround() {
+    ctx.fillStyle = "#6e6363";
+    ctx.fillRect(0, (0.8*board.height), board.width, (0.8*board.height));
+}
+function DrawCactus(xpos, cact_size_x, cact_size_y) {
+    ctx.fillStyle = "#2db700";
+    ctx.fillRect(xpos, (0.8*board.height), -1*cact_size_x, -1*cact_size_y)
+}
+function Game() {
+    DrawBoard();
+    DrawGround();
+    DrawCactus(cactus_xpos, 30, 120);
+    cactus_xpos -= 5;
+    if (cactus_xpos <= 0) {
+        cactus_xpos = board.width - 30;
+    }
+}
+setInterval(Game, 1);
